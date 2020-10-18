d9f498ec27d99fa01a1fd74332175ddd187e073d
Itilon <ivanov.elect@gmail.com>
2020-10-18 21:19:50 +0300

    Swipe functionality added and small screen gallery styles

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,5 @@
+window.xDown = null;
+
 document.addEventListener('DOMContentLoaded', () => {
     const main = document.querySelector('.main');
     const rightArrow = main.querySelector('.fa-arrow-circle-right');
@@ -10,7 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     rightArrow.addEventListener('click', rightSlide.bind(null, rightArrow, imageContainers));
     leftArrow.addEventListener('click', leftSlide.bind(null, leftArrow, imageContainers));
-    images.forEach(image => image.addEventListener('click', resizeImage.bind(null, image, main)));
+    images.forEach(image => {
+        image.addEventListener('click', resizeImage.bind(null, image, main));
+        image.addEventListener('touchstart', onTouchStart);
+        image.addEventListener('touchmove', onTouchMove.bind(null, imageContainers));
+    });
     imageWrapper.addEventListener('click', downsizeImage.bind(null, imageWrapper, main));
     imageOverlay.addEventListener('click', downsizeImage.bind(null, imageOverlay, main));
 });
@@ -124,4 +130,80 @@ const removeResizedClass = (main) => {
     });
 
    main.querySelectorAll('i.fa.hidden').forEach(elt => elt.classList.remove('hidden'));
+};
+
+const onTouchStart = () => {
+    this.xDown = this.event.touches[0].clientX;
+};
+
+const onTouchMove = (imageContainers) => {
+    let visibleImageChanged = false;
+
+    if (this.xDown) {
+        const xUp = this.event.touches[0].clientX;                                    
+
+        const xDiff = this.xDown - xUp;
+
+        if (xDiff > 0) {
+            imageContainers.forEach((container, i) => {
+                if (container.classList.contains(('visible-item')) && !visibleImageChanged) {
+                    container.classList.remove('visible-item');
+        
+                    const nextContainer = imageContainers.find(container => container.classList.contains('next-item'));
+                    nextContainer.classList.remove('next-item');
+                    nextContainer.classList.add('visible-item');
+        
+                    const previousContainer = imageContainers.find(container => container.classList.contains('previous-item'));
+                    previousContainer.classList.remove('previous-item');
+        
+                    container.classList.add('previous-item');
+                    
+                    if (imageContainers[i + 2]) {
+                        imageContainers[i + 2].classList.add('next-item');
+                    } else if (imageContainers[i + 1]) {
+                        imageContainers[0].classList.add('next-item');
+                    } else {
+                        imageContainers[1].classList.add('next-item');
+                    }
+                    
+                    const textContainers = document.querySelectorAll('.image-text-container');
+                    textContainers[i].classList.add('hidden');
+                    textContainers[i + 1] ? textContainers[i + 1].classList.remove('hidden') : textContainers[0].classList.remove('hidden');
+
+                    visibleImageChanged = true;
+                }
+            });
+        } else {
+            imageContainers.forEach((container, i) => {
+                if (container.classList.contains(('visible-item')) && !visibleImageChanged) {
+                    container.classList.remove('visible-item');
+        
+                    const previousContainer = imageContainers.find(container => container.classList.contains('previous-item'));
+                    previousContainer.classList.remove('previous-item');
+                    previousContainer.classList.add('visible-item');
+        
+                    const nextContainer = imageContainers.find(container => container.classList.contains('next-item'));
+                    nextContainer.classList.remove('next-item');
+        
+                    container.classList.add('next-item');
+                    
+                    if (imageContainers[i - 2]) {
+                        imageContainers[i - 2].classList.add('previous-item');
+                    } else if (imageContainers[i - 1]) {
+                        imageContainers[imageContainers.length - 1].classList.add('previous-item');
+                    } else {
+                        imageContainers[imageContainers.length - 2].classList.add('previous-item');
+                    }
+    
+                    const textContainers = document.querySelectorAll('.image-text-container');
+                    textContainers[i].classList.add('hidden');
+                    textContainers[i - 1] ? textContainers[i - 1].classList.remove('hidden') : textContainers[imageContainers.length - 1].classList.remove('hidden');
+        
+                    visibleImageChanged = true;
+                }
+            });
+        }
+
+        this.xDown = null;
+    }
 };
